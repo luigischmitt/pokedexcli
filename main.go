b539b8aa8be501c3f56400a317eb0395605e2bb6
luigischmitt <schmittluigi@gmail.com>
2026-04-16 15:45:11 -0300

    feat: add a buffer io in our cli pokedex and a gitignore, ignoring our logs

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 
-	fmt.Printf("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// infinite loop for REPL (read-eval-print loop)
+	for {
+		fmt.Print("Pokedex > ")
+		
+		if !scanner.Scan() {	// read til the next /n and return true or fakse
+			break
+		}
+
+		userInput := scanner.Text()
+		words := cleanInput(userInput)
+		if len(words) == 0 {	// if it is empty go to the next interation
+			continue
+		}
+
+		command := words[0]	// the command is the first word of the input
+		fmt.Printf("Your command was: %s\n", command)
+
+	}
 
 }
